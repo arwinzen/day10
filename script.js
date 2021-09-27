4b138bc205e778dac1119eb368c17ce5c07cddd7
rw1n <arwingoo90@gmail.com>
2021-09-28 00:41:05 +0800

    Added a border around each section and a linear gradient

--- script.js
+++ script.js
@@ -28,49 +28,25 @@ window.addEventListener('scroll', onScroll);
 function onScroll(evt){
     let currentWidth = window.scrollY / (container.scrollHeight - window.innerHeight) * 100;
     progressBar.style.width = currentWidth + '%';
-
-    if(window.scrollY >= section2.offsetTop){
-        console.log('you have reached section 2');
-        // itemHolder.style.display = 'flex';
-        section2.classList.remove('fadeOut');
-        section2.classList.add('fadeIn');
-
-    } else {
-        console.log('you are probably above section 2')
-        section2.classList.remove('fadeIn');
-        section2.classList.add('fadeOut');
-        // section2.style.opacity = 0;
-        // itemHolder.style.visibility = 'hidden';
-    }
-
-    // for(let i = 0; i < photoList.length; i++){
-    //     if (window.scrollY + 500 >= photoList[i].offsetTop){
-    //         photoList[i].classList.remove('fadeOut');
-    //         photoList[i].classList.add('fadeIn');
-    //     } else {
-    //         photoList[i].classList.remove('fadeIn');
-    //         photoList[i].classList.add('fadeOut');
-    //     }
-    // }
 };
 
 // section1 positions
-const section1Pos = {w:1000, h:861};
-layer1Pos = {x:0, y:0, w: 1000, h: 861};
-layer2Pos = {x:0, y:0, w: 1000, h: 861};
-layer3Pos = {x:0, y:0, w: 1000, h: 861};
-layer4Pos = {x:0, y:0, w: 1000, h: 861};
-layer5Pos = {x:0, y:0, w: 1000, h: 861};
-layer6Pos = {x:0, y:0, w: 1000, h: 861};
+const section1Pos = {w:1000, h:400};
+layer1Pos = {x:0, y:0, w: 1000, h: 400};
+layer2Pos = {x:0, y:0, w: 1000, h: 400};
+layer3Pos = {x:0, y:0, w: 1000, h: 400};
+layer4Pos = {x:0, y:0, w: 1000, h: 400};
+layer5Pos = {x:0, y:0, w: 1000, h: 400};
+layer6Pos = {x:0, y:0, w: 1000, h: 400};
 
 // section2 positions
 const section2Pos = {w:1000, h:861};
-photo1Pos = {x:0, y:0, w: 1000, h: 861};
-photo2Pos = {x:0, y:0, w: 1000, h: 861};
-photo3Pos = {x:0, y:0, w: 1000, h: 861};
-photo4Pos = {x:0, y:0, w: 1000, h: 861};
-photo5Pos = {x:0, y:0, w: 1000, h: 861};
-photo6Pos = {x:0, y:0, w: 1000, h: 861};
+photo1Pos = {x:0, y:0, w: 1200, h: 861};
+photo2Pos = {x:0, y:0, w: 1200, h: 861};
+photo3Pos = {x:0, y:0, w: 1200, h: 861};
+photo4Pos = {x:0, y:0, w: 1200, h: 861};
+photo5Pos = {x:0, y:0, w: 1200, h: 861};
+photo6Pos = {x:0, y:0, w: 1200, h: 861};
 
 window.addEventListener('mousemove', onMouseMove);
 
@@ -79,22 +55,22 @@ function onMouseMove(evt){
     // console.log(`evt.pageX : ${evt.pageX}, containerOffSet: ${section1.offsetLeft}`, `mouseX: ${mouse.x}`);
 
     // section 1 layers
-    layer1Pos.x = (-mouse.x * 0.08);
+    layer1Pos.x = (-mouse.x * 0.08) + (section2Pos.w / 2 - layer1Pos.w / 2);
     layer1.style.left = layer1Pos.x + 'px';
 
-    layer2Pos.x = (-mouse.x * 0.05);
+    layer2Pos.x = (-mouse.x * 0.05) + (section2Pos.w / 2 - layer1Pos.w / 2);
     layer2.style.left = layer2Pos.x + 'px';
 
-    layer3Pos.x = (-mouse.x * 0.03);
+    layer3Pos.x = (-mouse.x * 0.03) + (section2Pos.w / 2 - layer1Pos.w / 2);
     layer3.style.left = layer3Pos.x + 'px';
 
-    layer4Pos.x = (-mouse.x * 0.01);
+    layer4Pos.x = (-mouse.x * 0.01) + (section2Pos.w / 2 - layer1Pos.w / 2);
     layer4.style.left = layer4Pos.x + 'px';
 
-    layer5Pos.x = (-mouse.x * 0.008);
+    layer5Pos.x = (-mouse.x * 0.008) + (section2Pos.w / 2 - layer1Pos.w / 2);
     layer5.style.left = layer5Pos.x + 'px';
 
-    layer6Pos.x = (-mouse.x * 0.006);
+    layer6Pos.x = (-mouse.x * 0.005) + (section2Pos.w / 2 - layer1Pos.w / 2);
     layer6.style.left = layer6Pos.x + 'px';
 
     //section 2 layers
